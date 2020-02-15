3b98cdfc1aaa3f3938aaba957bf4d4f20d0de4c8
mayben0t <594180536@qq.com>
2020-02-15 21:00:13 +0800

    project init

---
+++ src/main/java/com/example/first_backend/FirstBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.first_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/first_backend/FirstBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.first_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
